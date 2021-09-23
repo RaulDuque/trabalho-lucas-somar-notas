0bbcfbb2ce965188451191b576ce38cb10898f53
RaulDuque <raulgduque2000@gmail.com>
2021-09-23 19:00:37 -0300

    Couter app Lucas

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu Primeiro app"),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(),
+          TextField(),
+          TextField(),
+          TextField(),
+          ElevatedButton(
+            child: Text('Caucular'),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
